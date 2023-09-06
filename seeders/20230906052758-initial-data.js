f2b12481a28ac292d5e62e6e06d9a05aaae47228
xrchitron <yuahochen1124@gmail.com>
2023-09-06 14:28:50 +0800

    build: generate seed data to database

--- seeders/20230906052758-initial-data.js
+++ seeders/20230906052758-initial-data.js
@@ -1,25 +1,12 @@
-'use strict';
-
+"use strict";
+const restaurant = require("../public/jsons/restaurant.json");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("restaurants", restaurant.results, {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("restaurants", null, {});
+  },
 };
